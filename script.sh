51868511dfab387f7f172f10c5fcf62719cb73db
Thikpad t480 <idwinursid@gmail.com>
2024-02-23 09:56:49 +0700

    first commit

---
+++ script.sh
@@ -0,0 +1,6 @@
+# hapus semua file di folder templates
+rm -rf templates/*
+
+# generate manifests
+helm template my-app . --set=image.repository=nginx --set=image.tag=alpine --set=autoscaling.minReplicas=5 --set=autoscaling.maxReplicas=1000 --set=autoscaling.targetCPUUtilizationPercentage=50 --set=autoscaling.targetMemoryUtilizationPercentage=100
+helm lint .
